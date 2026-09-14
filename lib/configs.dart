0343ea5cb04efa8bd48e6814ff3a34aa94c9a9cb
Luanjinx <sahruldarmian97@gmail.com>
2026-09-13 22:34:57 -0700

    Initial Commit

---
+++ lib/configs.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:io';
+
+import 'package:country_picker/country_picker.dart';
+import 'package:streamit_laravel/generated/assets.dart';
+
+const APP_NAME = 'Nexstream';
+const APP_MINI_LOGO_URL = Assets.assetsAppMiniLogo;
+const APP_LOGO_URL = Assets.assetsAppLoader;
+const APP_FONT_FAMILY = 'Roboto';
+const DEFAULT_LANGUAGE = 'en';
+const AUTO_SLIDER_SECOND = 6000;
+const CUSTOM_AD_AUTO_SLIDER_SECOND_VIDEO = 30000;
+const CUSTOM_AD_AUTO_SLIDER_SECOND_IMAGE = 30000;
+const LIVE_AUTO_SLIDER_SECOND = 5;
+
+const API_VERSION = 3;
+
+///DO NOT ADD SLASH HERE
+String DOMAIN_URL = 'https://app.ryzprjkt.cloud';
+
+bool isInReview = false;
+
+String get baseUrlKeyName => Platform.isAndroid ? 'baseUrl' : 'iosBaseUrl';
+
+String get BASE_URL => '$DOMAIN_URL/api/';
+
+const APP_APPSTORE_URL = '';
+const APP_PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.iqonic.streamitlaravel';
+
+///LOCAL VIDEO TYPE URL
+String get LOCAL_VIDEO_DOMAIN_URL => '$DOMAIN_URL/storage/streamit-laravel/';
+
+//Note: For FIREBASE_CLIENT_ID ---> Go to android/app/google-services.json
+// - Find press ctrl+F and look for "client_type": 3
+// "client_id" in same object has be pasted here
+
+String get FIREBASE_CLIENT_ID => Platform.isAndroid ? '988658912152-fa0ouj7at8jubc2kgl5rnq0noilke89i.apps.googleusercontent.com' : 'YOUR_IOS_FIREBASE_CLIENT_ID';
+
+//region STRIPE
+const STRIPE_URL = 'https://api.stripe.com/v1/payment_intents';
+const STRIPE_merchantIdentifier = "merchant.flutter.stripe.test";
+const STRIPE_MERCHANT_COUNTRY_CODE = 'IN';
+const STRIPE_CURRENCY_CODE = 'INR';
+//endregion
+
+//region RazorPay
+const String commonSupportedCurrency = 'INR';
+//endregion
+
+//region  PAYSTACK
+const String payStackCurrency = "NGN";
+//endregion
+
+// PAYPAl
+const String payPalSupportedCurrency = 'USD';
+//endregion
+
+//ADs
+//Android
+const INTERSTITIAL_AD_ID = "ca-app-pub-3940256099942544/1033173712";
+const BANNER_AD_ID = "ca-app-pub-3 940256099942544/9214589741";
+//IOS
+const IOS_INTERSTITIAL_AD_ID = "ca-app-pub-3940256099942544/4411468910";
+const IOS_BANNER_AD_ID = "ca-app-pub-3940256099942544/2934735716";
+
+//region defaultCountry
+Country get defaultCountry => Country(
+      phoneCode: '62',
+      countryCode: 'ID',
+      e164Sc: 62,
+      geographic: true,
+      level: 1,
+      name: 'Indonesia',
+      example: '23456789',
+      displayName: 'Indonesia (ID) [+62]',
+      displayNameNoCountryCode: 'Indonesia (ID)',
+      e164Key: '62-ID-0',
+      fullExampleWithPlusSign: '+628123456789',
+    );
+//endregion
---
+++ lib/firebase_options.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCsJLRihuO-i2cMfJeB7lLeCJ158e8yeFk',
+    appId: '1:988658912152:android:9a4bf78d663e738a7bf9ed',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    projectId: 'streamit-5ef16',
+    storageBucket: 'streamit-5ef16.firebasestorage.app',
+    iosBundleId: 'YOUR_IOS_BUNDLE_ID'
+  );
+
+  ///Note : Values available android/app/google-services.json
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: '1:988658912152:android:9a4bf78d663e738a7bf9ed',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    projectId: 'streamit-5ef16',
+    storageBucket: 'streamit-5ef16.firebasestorage.app',
+    iosBundleId: 'YOUR_IOS_BUNDLE_ID'
+  );
+}
