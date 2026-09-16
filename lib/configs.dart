3eb0c20b8ef62fd245f9d4db85505d26536b7186
Luanjinx <sahruldarmian97@gmail.com>
2026-09-16 12:32:04 -0700

    Update configs.dart

--- lib/configs.dart
+++ lib/configs.dart
@@ -5,7 +5,7 @@ import 'dart:io';
 import 'package:country_picker/country_picker.dart';
 import 'package:streamit_laravel/generated/assets.dart';
 
-const APP_NAME = 'Nexstream';
+const APP_NAME = 'Streamit';
 const APP_MINI_LOGO_URL = Assets.assetsAppMiniLogo;
 const APP_LOGO_URL = Assets.assetsAppLoader;
 const APP_FONT_FAMILY = 'Roboto';
@@ -18,11 +18,11 @@ const LIVE_AUTO_SLIDER_SECOND = 5;
 const API_VERSION = 3;
 
 ///DO NOT ADD SLASH HERE
-String DOMAIN_URL = 'https://app.ryzprjkt.cloud';
+String DOMAIN_URL = "https://app.nexstream.biz.id";
 
 bool isInReview = false;
 
-String get baseUrlKeyName => Platform.isAndroid ? 'baseUrl' : 'iosBaseUrl';
+String get baseUrlKeyName => Platform.isAndroid ? 'developingBaseUrl' : 'developingBaseUrl';
 
 String get BASE_URL => '$DOMAIN_URL/api/';
 
@@ -36,7 +36,7 @@ String get LOCAL_VIDEO_DOMAIN_URL => '$DOMAIN_URL/storage/streamit-laravel/';
 // - Find press ctrl+F and look for "client_type": 3
 // "client_id" in same object has be pasted here
 
-String get FIREBASE_CLIENT_ID => Platform.isAndroid ? '988658912152-fa0ouj7at8jubc2kgl5rnq0noilke89i.apps.googleusercontent.com' : '988658912152-fa0ouj7at8jubc2kgl5rnq0noilke89i.apps.googleusercontent.com';
+String get FIREBASE_CLIENT_ID => Platform.isAndroid ? '988658912152-78q6biuf9lklfb4tmju9f303bn0cdm4i.apps.googleusercontent.com' : '988658912152-fa0ouj7at8jubc2kgl5rnq0noilke89i.apps.googleusercontent.com';
 
 //region STRIPE
 const STRIPE_URL = 'https://api.stripe.com/v1/payment_intents';
@@ -58,25 +58,28 @@ const String payPalSupportedCurrency = 'USD';
 //endregion
 
 //ADs
-//Android
+//Live keys
+// //Android
 const INTERSTITIAL_AD_ID = "ca-app-pub-3940256099942544/1033173712";
 const BANNER_AD_ID = "ca-app-pub-3 940256099942544/9214589741";
-//IOS
+
+// //IOS
 const IOS_INTERSTITIAL_AD_ID = "ca-app-pub-3940256099942544/4411468910";
 const IOS_BANNER_AD_ID = "ca-app-pub-3940256099942544/2934735716";
 
+
 //region defaultCountry
 Country get defaultCountry => Country(
-      phoneCode: '62',
-      countryCode: 'ID',
-      e164Sc: 62,
+      phoneCode: '91',
+      countryCode: 'IN',
+      e164Sc: 91,
       geographic: true,
       level: 1,
-      name: 'Indonesia',
+      name: 'India',
       example: '23456789',
-      displayName: 'Indonesia (ID) [+62]',
-      displayNameNoCountryCode: 'Indonesia (ID)',
-      e164Key: '62-ID-0',
-      fullExampleWithPlusSign: '+628123456789',
+      displayName: 'India (IN) [+91]',
+      displayNameNoCountryCode: 'India (IN)',
+      e164Key: '91-IN-0',
+      fullExampleWithPlusSign: '+919123456789',
     );
 //endregion
